d61c2eec96c472519135337e836f01320e6de6a8
Alex <46695038+munetta@users.noreply.github.com>
2023-12-25 15:51:03 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -3,27 +3,19 @@ hello world
 1. the unique point on a graph via its input set is used to create a regression model from scratch
 2. model points can all be represented on a single axis (transforming lines into single unique points can be done algorithmically at chosen INTERVALS) ex: video model second model
 
-REGRESSION MODEL 
+REGRESSION PICTURE MODEL 
 question: a model to tell if a basketball is in a picture
 transforming line: no
-example: picture 
 parameters: pixel_location, pixel_color
 algorithm: pixel_location * pixel_color + ...n
 
-REGRESSION MODEL
-question: a model to tell if a basketball goes trough a hoop (must observe how many points are necessary to observe correct video)
-line duration transformed: 18 y points... one point representing a millisecond
-tansforming line: yes  ...transofrm 18 point lines into one point, defined uniquely (the beginning and end of the interval can be sed numerically to define uniqueness)
-example: video
+REGRESSION VIDEO MODEL
+question: a model to tell if a basketball goes through a hoop (must observe how many points are necessary to identify correct video)
+line duration transformed: 18 y points... one point representing a millisecond and a frame 18 frames tied together to hopefully represent a basketball going through a hoop
+tansforming line: yes  ...transofrm 18 point incoming lines into one point, defined uniquely, to compare with the other points already transformed and plotted (the beginning to end of the lines, each individual node, can be used to transform)
 parameters: pixel_location, pixel_color
 algorithm: pixel_location * pixel_color + ...n
 
-every 18 points, transform the line into a single point
+every 18 points, transform the line into a single point, plot... for incoming do the same
 
 make sure to label the points with large zoom conferences 
-
-this point seems to be for basketballs
-
-(or do the algorithm of your choice and compare that way)
-
-done
