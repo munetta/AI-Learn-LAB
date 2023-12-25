6c4b293af6c4f87f008b4c9c79e6d1179eb17b88
Alex <46695038+munetta@users.noreply.github.com>
2023-12-25 16:32:34 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -1,7 +1,7 @@
 hello world
 
 1. the unique point on a graph via its input set (parameters/constants and features/variables however do not confuse what is necessary and not, uniqueness is the goal) is used to create a regression model from scratch
-2. model points can all be represented on a single axis (transforming lines into single unique points can be done algorithmically at chosen INTERVALS/between y values) ex: video model second model <-- transforming a line into a single point makes it easier to compare incoming targets
+2. model points can all be represented on a single axis (transforming lines into single unique points can be done algorithmically at chosen INTERVALS/between y values) ex: video model second model <-- transforming a line into a single point makes it easier to compare incoming targets <-- if not transforming line, comparisons of incoming lines can be done via a regular regression mean formula
 
 REGRESSION PICTURE MODEL 
 question: a model to tell if a basketball is in a picture
