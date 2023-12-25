14506fefb403430a990c477fb136b7cd38e360cc
Alex <46695038+munetta@users.noreply.github.com>
2023-12-25 18:47:51 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -11,7 +11,7 @@ algorithm: pixel_location * pixel_color + ...n
 
 REGRESSION VIDEO MODEL
 goal: a model to tell if a basketball goes through a hoop (must observe how many frames/xPoints/ms are necessary to identify correct video)
-line duration transformed: 18 y points... one point representing a millisecond and a frame 18 frames tied together to hopefully represent a basketball going through a hoop (saved for incoming targets) -- saved as unique point or as a line (unique point for easier comparison)
+line duration transformed: 18 x points... one point representing a millisecond and a frame 18 frames tied together to hopefully represent a basketball going through a hoop (saved for incoming targets) -- saved as unique point or as a line (unique point for easier comparison)
 transforming line: yes  ...transoform 18 point incoming lines into uniquely defined points, and compare with the other lines already transformed and plotted unique points (the beginning to end of the lines, each individual node, can be used algorithmically to transform the line into a single unique point)
 parameters: pixel_location, pixel_color
 algorithm: pixel_location * pixel_color + ...n
