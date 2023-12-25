df159bf7233c9da6d0a289bfb55524d3ed1d27a9
Alex <46695038+munetta@users.noreply.github.com>
2023-12-25 11:23:21 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -1,1 +1,29 @@
 hello world
+
+1. the unique point on a graph via its input set is used to create a regression model from scratch
+2. model points can all be represented on a single axis' (transforming lines into single unique points can be done algorithmically at chosen INTERVALS) ex: video model second model
+
+REGRESSION MODEL 
+question: a model to tell if a basketball is in a picture
+transforming line: no
+example: picture 
+parameters: pixel_location, pixel_color
+algorithm: pixel_location * pixel_color + ...n
+
+REGRESSION MODEL
+question: a model to tell if a basketball goes trough a hoop (must observe how many points are necessary to observe correct video)
+line duration transformed: 18 y points... one point representing a millisecond
+tansforming line: yes  ...transofrm 18 point lines into one point, defined uniquely (the beginning and end of the interval can be sed numerically to define uniqueness)
+example: video
+parameters: pixel_location, pixel_color
+algorithm: pixel_location * pixel_color + ...n
+
+every 18 points, transform the line into a single point
+
+make sure to label the points with large zoom conferences 
+
+this point seems to be for basketballs
+
+(or do the algorithm of your choice and compare that way)
+
+done
