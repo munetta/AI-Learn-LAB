08a13e81467eb781df93f55b65415460a879198e
Alex <46695038+munetta@users.noreply.github.com>
2023-12-25 16:02:15 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -1,7 +1,7 @@
 hello world
 
 1. the unique point on a graph via its input set is used to create a regression model from scratch
-2. model points can all be represented on a single axis (transforming lines into single unique points can be done algorithmically at chosen INTERVALS) ex: video model second model
+2. model points can all be represented on a single axis (transforming lines into single unique points can be done algorithmically at chosen INTERVALS) ex: video model second model <--
 
 REGRESSION PICTURE MODEL 
 question: a model to tell if a basketball is in a picture
@@ -10,12 +10,12 @@ parameters: pixel_location, pixel_color
 algorithm: pixel_location * pixel_color + ...n
 
 REGRESSION VIDEO MODEL
-question: a model to tell if a basketball goes through a hoop (must observe how many points are necessary to identify correct video)
+question: a model to tell if a basketball goes through a hoop (must observe how many frames/yPoints/ms are necessary to identify correct video)
 line duration transformed: 18 y points... one point representing a millisecond and a frame 18 frames tied together to hopefully represent a basketball going through a hoop
 tansforming line: yes  ...transofrm 18 point incoming lines into one point, defined uniquely, to compare with the other lines already transformed and plotted as single points (the beginning to end of the lines, each individual node, can be used to transform into a single point uniquely)
 parameters: pixel_location, pixel_color
 algorithm: pixel_location * pixel_color + ...n
 
-every 18 points, transform the line into a single point, plot... for incoming do the same and just compare. real problem is dividing up all the interval times. some 18 frames some 40 frames depending on what you are looking for
+every 18 points, transform the line into a single point, plot... for incoming do the same, then compare (with the up down counting pattern). real problem is dividing up all the interval times. some 18 frames some 40 frames depending on what you are looking for
 
 make sure to label the points with large zoom conferences in the beginning, check for accuracy over a short meeting.
