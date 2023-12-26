36c978c641cf3364a9f9f8570c5cd53325764d62
Alex <46695038+munetta@users.noreply.github.com>
2023-12-26 10:20:33 -0500

    Update picture.js

--- picture.js
+++ picture.js
@@ -1,1 +1,10 @@
 this is a picture model written in javascript. there are three versions. one with a line. one with a point. one with a line converted to a point. all displayed.
+
+  order 
+  point 
+  line 
+  line to point 
+
+  proof 
+  can lines be oonverted back to points affectively (uniquely)?
+  assumption: yes
