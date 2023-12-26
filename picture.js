13a0499ec6af41781cdab8064d977f552ab37dac
Alex <46695038+munetta@users.noreply.github.com>
2023-12-26 10:57:11 -0500

    Update picture.js

--- picture.js
+++ picture.js
@@ -1,10 +1,36 @@
 this is a picture model written in javascript. there are three versions. one with a line. one with a point. one with a line converted to a point. all displayed.
 
   order 
-  point 
-  line 
-  line to point 
+  1. point 
+  2. line 
+  3. line to point 
 
   proof 
-  can lines be oonverted back to points affectively (uniquely)?
+  can lines be converted back to points affectively (uniquely)?
   assumption: yes
+
+  1. point
+
+  let graph = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  ];
+
+  let picture = 'path-to-image.jpg'; 
+
+  let x = 0; 
+  let pixel_location_differential = 2;
+
+  for(let i = 0; i  picture.length; i++) {
+    
+  }
+  
+
+
