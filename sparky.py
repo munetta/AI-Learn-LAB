ebd373cbaedf708ee42d301c935aa278c68a8c21
Alex <46695038+munetta@users.noreply.github.com>
2023-12-25 21:36:59 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -2,7 +2,7 @@ hello world
 
 Alex E
 
-1. the unique point on a graph via its input set (features) is used to create a regression model from scratch. Points eventually becoming lines (measured in intervals), however, lines being able to convert back into single unique points for labeling (if necessary). Regression mean formulas are used on lines for comparing incoming target lines. Iteritive addidive comparing is used on single axis points to compare incoming target points. Regression mean formulas on lines, also can represent variance (which is why i like converting  lines to single unique points --- easier comparing --- lighter computation possibly)  
+1. the unique point on a graph via its input set (features) is used to create a regression model from scratch. Points (in some cases) eventually becoming lines (measured in intervals), however, lines being able to convert back into single unique points for labeling (if necessary). Regression mean formulas are used for comparing incoming target lines. Iteritive addidive comparing is used for comparing incoming target points. Regression mean formulas on lines, also can represent variance (which is why i like converting  lines to single unique points --- easier comparing --- lighter computation possibly)  
 2. model points can all be represented on a single axis (transforming lines into single unique points can be done algorithmically from start to finish of line) ex: video model second model <-- transforming a line into a single point makes it easier to compare incoming targets <-- if not transforming a line, comparisons of incoming lines can be done via a regular regression mean formula.
 
 -parameter should be a 'constant local differential' (to help define input set uniquely) <-- in this case pixel_location is okay
@@ -11,7 +11,7 @@ why convert a line to a single point on a server for labeling.
 -avoid the regression formula running on servers - however, still need to run 'a' formula just without division...
 -might still be slower though
 
-MIN POINT IS SOME MODELS MIGHT NEED LINES WHILE OTHERS DO NOT - however if lines are neccessary, you can convert them into points
+MAIN POINT IS SOME MODELS MIGHT NEED LINES WHILE OTHERS DO NOT - however if lines are neccessary, you can convert them into points to compare in a more friendly manner
                                                                                                                                                                                                                                                                                                                          
 REGRESSION PICTURE MODEL 
 goal: a model to tell if a basketball is in a picture
@@ -59,8 +59,6 @@ pixel_1     ...t (the target is all the features added up) option one: added on 
 -------
 pixel_color*
 pixel_location (local differential parameter)
-
-
 
 (they say a parameter is a constant in a formula)
 
