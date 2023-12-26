4d6f64e3e4c6299c3391e711fb652dd427609301
Alex <46695038+munetta@users.noreply.github.com>
2023-12-25 19:12:31 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -1,6 +1,6 @@
 hello world
 
-1. the unique point on a graph via its input set (parameters/constants and features/variables however do not confuse what is necessary and not, uniqueness is the goal) is used to create a regression model from scratch. Points eventually becoming lines, however, can be converted back into single unique points. 
+1. the unique point on a graph via its input set (parameters/constants and features/variables however do not confuse what is necessary and not, uniqueness is the goal) is used to create a regression model from scratch. Points eventually becoming lines, however, lines being able to convert back into single unique points. 
 2. model points can all be represented on a single axis (transforming lines into single unique points can be done algorithmically at chosen INTERVALS/between x values) ex: video model second model <-- transforming a line into a single point makes it easier to compare incoming targets <-- if not transforming line, comparisons of incoming lines can be done via a regular regression mean formula
 
 why convert a line to a single point on a server
