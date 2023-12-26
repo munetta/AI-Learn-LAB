f07e4bdc927152c1883a0a9350342127d215fd5b
Alex <46695038+munetta@users.noreply.github.com>
2023-12-26 09:43:09 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -3,7 +3,7 @@ hello world
 Alex E
 
 The unique point on a graph coming from an input set (features) is used to create a regression model from scratch. The algorithm acting on the input set must add to a unique point. (so far a categorical constant between features plotted seperately along the x has shown uniqueness) <-- with the conversion process
-Points eventually becoming lines (if neccessary, measured in intervals), however, lines being able to convert back into single unique points for labeling (if necessary) and for faster comparing when an incoming target comes in (efficency). <-- this might be wrong -- have to compare the regression variance algorithms with the conversion algorithm -> then adding entire number set and dividing by set amount
+Points eventually becoming lines (if neccessary, measured in intervals), however, lines being able to convert back into single unique points for labeling (if necessary) and for faster comparing when an incoming target comes in (efficency). <-- this might be wrong -- have to compare the regression min distance algorithms with the conversion algorithm -> then adding entire number set and dividing by set amount
 Regression formulas can be used for comparing stored unique lines, with incoming target lines (inefficent but possibly much more accurate labeling). -- The regression formulas always can indictate the center point variance for targets (excuse wording). 
 Iteritive addidive comparing is used for comparing incoming target points (more efficient but if not done correctly, less accurate labeling). <--- converting line here (if line present)
 
