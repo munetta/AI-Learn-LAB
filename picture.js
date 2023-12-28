36e53ce454c7fe3f4b0f4dd5a73a5d7a6d0c84be
Alex <46695038+munetta@users.noreply.github.com>
2023-12-27 22:44:29 -0500

    Update picture.js

--- picture.js
+++ picture.js
@@ -1,16 +1,7 @@
-this is a picture model written in javascript. there are three versions. one with a line. one with a point. one with a line converted to a point. all displayed.
+  this is a picture model written in javascript.
 
-  order 
-  1. point -- this will display a unique point via an added unique number
-  2. line -- this will display a unique line along the x axis <--- each feature denoted with a unique number defines the uniqueness of the point then line 
-  3. line to point -- algorithm to convert an incoming line, to a point (xi * yi + ...n) 
-
-  proof 
-  can lines be converted back to points affectively (uniquely)?
-  assumption: yes
-
-  how
-  using the line-point algorithm
+  goal 
+  accurately denote a picture using a line drawn across (label the line) (convert the line to a unique point)
 
   1. point - this is taking a picture and plotting it on a single axis
 
