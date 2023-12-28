6b40cec8eaa2c5a83c2dc95acd8038d3b30a9f2d
Alex <46695038+munetta@users.noreply.github.com>
2023-12-28 07:52:18 -0500

    Update picture.js

--- picture.js
+++ picture.js
@@ -1,9 +1,9 @@
   this is a picture model written in javascript.
 
   goal 
-  accurately denote a picture using a line drawn across (label the line) (convert the line to a unique point)
+  accurately denote a picture using a line drawn across a graph (label the line) (convert the line to a unique point and comare how easy it is to compare incoming targets)
 
-  1. point - this is taking a picture and plotting it on a single axis
+  why? turning a picture into a unique line is easier to compare than the picture itself which cant be done unless converted. 
 
   let graph = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
