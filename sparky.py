de3d4a52eeefee7e60cf4ee47d41c06691f45ae5
Alex <46695038+munetta@users.noreply.github.com>
2023-12-28 12:27:11 -0500

    Update sparky.py

--- sparky.py
+++ sparky.py
@@ -2,8 +2,8 @@ Alex E
 Notes
 
 Topic 
-Each feature represents a new point on the line. (ex feature: pixel density (black white scale), plotted, move next)
-after plotting, converting into a point to denote the entire line (xi * yi ...) <-- this is the proof <--however  comparing points isnt as diverse of a process than comparing lines
+-Each feature represents a new point on the line. (ex feature: pixel density (black white scale), plotted, move next)
+-After plotting, converting into a point to denote the entire line (xi * yi ...) <-- this is the proof <--however  comparing points isnt as diverse of a process than comparing lines (storing and comparing lines vs storing and comparing points))
                                                                                                                                                                                                                                                                                                                          
 REGRESSION PICTURE MODEL 
 goal: a model to tell if a basketball is in a picture
@@ -32,6 +32,6 @@ algorithm: pixel_location * pixel_color + ...n
 video.js   
                                                                                                                                                                                                                                                 
 conclusion
-a line drawn along the x can denote uniqueness... just add the attributic set per feature, plot and move next. The attributic set being all the parameters describing with the feature.
+a line drawn along the x can denote uniqueness... just add the attributic set per feature, plot and move next. The attributic set being all the parameters describing the feature.
 once the line is drawn, convert it into a single point using (xi * yi ...n) - this should be the unique point of the line, however, comparison methods on the line seem nicer
                                                                                                                                                                                                                                                                                                                                           
