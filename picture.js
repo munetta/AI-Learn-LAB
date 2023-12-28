299a9d0bd2620463989f46b7d187917b97e1d06a
Alex <46695038+munetta@users.noreply.github.com>
2023-12-28 08:04:12 -0500

    Update picture.js

--- picture.js
+++ picture.js
@@ -3,8 +3,6 @@
   goal 
   accurately denote a picture using a line drawn across a graph (label the line) (convert the line to a unique point and comare how easy it is to compare incoming targets)
 
-  why? turning a picture into a unique line is easier to compare than the picture itself which cant be done unless converted. 
-
   let graph = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
