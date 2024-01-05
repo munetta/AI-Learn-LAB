df7fa3fa8e67461dd44bf863818fc3a53ca9d750
Alex <46695038+munetta@users.noreply.github.com>
2024-01-05 12:16:24 -0500

    Update picture.js

--- picture.js
+++ picture.js
@@ -17,15 +17,15 @@
      -subtract feature points between lines, add and take the lowest value
      -the lowest value used for statistical reasoning. *
   3. compare every combinational slope between lines <-- neccessary and will show the true closeness of two lines
-     -(same process as two) <-- except feature slopes
-     -taking all combinations of slopes within a unique line. comparing the returned array against other lines sloped arrays. getting difference per feature adding to a min, which is compared against others. <-- creating an array of slopes (in n^2), and comparing (getting difference --> min) to another set (in the database)
-     -algorithm run on the outline of blackish pixels, each black pixel, denoted, as (iteritive multiple count), anything else, and the line continues without a plot. (most accurate for now)
-     -must select closest array line lengths which are already stored for comparing the incoming target line. (added difference between arrays <-- min selected)
+     -taking all combinations of slopes within a unique line. comparing the returned array against other lines sloped arrays. getting difference per index slope, adding to a min, which is compared against others. <-- creating an array of slopes (in n^2), and comparing (getting difference --> min) to another set (in the database)
+     -algorithm run on the outline of blackish pixels, each black pixel, denoted, as (unique iteritive multiple count), anything else, and the line continues without a plot. (most accurate for comparing two unique identifiable things)
+     -on search, must select closest array line lengths which are already stored for comparing the incoming target line. (added difference between arrays <-- min selected <--already noted right above)
+     -comparison of min used for statistical reasoning *
   4. increasing and decreasing lines along the y axis, to compare to other lines
      -3 negates this
      -this can be done before each deep comparison of lines maybe
   5. rotating pixels within set to run model on. getting everything there.
-     -3 negates this i think
+     -3 negates this
 
   //pushing all possible slope subtracted values to both arrays 
 
