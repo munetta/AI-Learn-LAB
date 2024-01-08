a5c6ab52745118f9672c72f76aab33d15d1f935f
Alex <46695038+munetta@users.noreply.github.com>
2024-01-08 11:40:41 -0500

    Update sparky.js

--- sparky.js
+++ sparky.js
@@ -6,17 +6,18 @@
   compare to other arrays (you can do this wholistically or compare via each pixel in pixel slope object)
 
   stored_unique_lines: all pictures stored via a unique line of grayish to black pixels
-  slope_arrays: converting each unique line to slope
-  graph: to show the target line on the graph
+  slope_arrays: converting each unique line to a slope array
+  graph: to show the target line on the graph 
   standard_y: putting everything right in the middle. the varying pixel color/number increasing and decreasing at this point
   pixel_slope_object: when drawing the line, each real point plotted, will be denoted in this as (x-y) (iterable difference starts at bottom left) (array gets larger, then smaller)
 */
 
 let stored_unique_lines = fetch_array_of_stored_line_arrays(); 
 let slope_arrays = convert_stored_line_arrays_to_slope_combinations(stored_lines)
-const image_length = 500;
 let pixel_slope_object = {};
 let graph = [];
+
+const image_length = 500;
 
 for(let i = 0; i < image_length; i++) {
     graph[i] = []; 
@@ -38,10 +39,11 @@ function image_intake(file_name)  {
     image = convert_to_iterable_array(image)
 
     /*
-        target picture comes in, picture converted to a unique line (white pixels skipped)
+        picture converted to a unique line on the graph
+        x_axis_iterator increases every pixel. white pixels arent plotted. (iteritive difference) <-- can compare against the actual difference
+        
     */
     
-   let target_line = [];
    let x_axis_iterator = 0
 
     /*
@@ -49,7 +51,7 @@ function image_intake(file_name)  {
     */
 
     for(let i = 0; i < image.length; i++) {
-        //whatever numbers are gray to black
+        //whatever numbers are gray to black 
         if(image[i].color === 'gray' || image[i].color === 'black') {
             //this is for exact pixel matching... 
             pixel_slope_object[`${image[i].x-image[i].y}`] = [];
@@ -58,22 +60,6 @@ function image_intake(file_name)  {
         x_axis_iterator += 1;
     }
     
-    for pixels in image 
-        if pixels[i].color > 5
-           target_line.append(pixels[i].color)
-        x_axis_iterator += 1 
-
-    /* 
-        all slopes drawn from target line
-    */
-    
-    target_slopes = []
-
-    for i in target_line 
-       point_one = target_line[i]
-          for j = i + 1 in target_line
-            point_two = target_line[j]
-            #get slope between point one and point two and store in target_slopes (subtracting slopes next)
 
 }
 
