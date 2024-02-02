182ef29240077160224a891a78b9d79915a53d1d
Alex <46695038+munetta@users.noreply.github.com>
2024-02-02 17:41:25 -0500

    Update sparky.js

--- sparky.js
+++ sparky.js
@@ -4,6 +4,7 @@
     University: WPI
     Department: Artificial Intelligence
     Title: Image wrap and matching algorithm (ARTIFICIAL INTELLIGENCE ALGORITHM)
+    Student Body: Black American Scientists
     Inspiration: Sam Altman
 */
 
