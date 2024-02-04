1f6ed3f3e8a1732efce285cb553fddbc7501e983
Alex <46695038+munetta@users.noreply.github.com>
2024-02-04 09:50:11 -0500

    Update sparky.js

--- sparky.js
+++ sparky.js
@@ -3,7 +3,7 @@
     Title: Picture algorithm
     Description: labels all edges of an image and uses the unique set of edges to label a picture
     Author: Alexander Eatman 
-    Student Body: Latin Black American Scientists Community
+    Student Body: Latin Black American Scientists
 */
 
 let edges = [];
